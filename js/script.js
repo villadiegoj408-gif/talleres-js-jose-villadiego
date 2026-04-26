cc5b095e97a81734828a41906b6ba22868bc9388
villadiegoj408-gif <villadiegoj408@gmail.com>
2026-04-26 11:53:58 -0500

    feat: ejercicio 1 declarar variables

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// ejercicio 1: declarar variables
+
+let nombre = "jose villadiego";
+let edad = 27;
+let estoyEstudiandoJS = true;
+ console.log("Hola mi nombre es " + nombre + " y tengo " + edad + " años de edad y la respuesta a la pregunta '¿estoy estudiando JS?' es " + estoyEstudiandoJS);
