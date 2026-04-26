62a03b3cef248c9886b0aa9fe4864750320dfa26
villadiegoj408-gif <villadiegoj408@gmail.com>
2026-04-26 12:18:14 -0500

    feat: ejercicio 2 array y objeto

--- js/script.js
+++ js/script.js
@@ -4,3 +4,16 @@ let nombre = "jose villadiego";
 let edad = 27;
 let estoyEstudiandoJS = true;
  console.log("Hola mi nombre es " + nombre + " y tengo " + edad + " años de edad y la respuesta a la pregunta '¿estoy estudiando JS?' es " + estoyEstudiandoJS);
+
+ // ejercicio 2: array y objeto
+
+ let comidasFavoritas = ["pizza", "pasta", "quesos", "lacteos", "carnes"];
+
+
+ let producto = {
+  nombre: "Laptop",
+  precio: 2500,
+  disponible: true
+};
+console.log("Comidas favoritas:", comidasFavoritas);
+console.log("Producto:", producto);
